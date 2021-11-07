63d479253dd52bc393678dcef8dd02d75d89cff6
DELL <saharakbeek@hotmail.com>
2021-11-07 13:37:50 -0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(child: Row(
+      children: <Widget>[
+        Expanded(
+            child: TextButton(onPressed: () {
+            print('left pressed');
+            },
+            child: Image.asset('images/dice1.png')),
+            ),
+        Expanded(
+            child: TextButton(onPressed: () {
+            print('right pressed');
+            },
+            child: Image.asset('images/dice1.png')),
+            )
+      ],
+    ),);
   }
 }
